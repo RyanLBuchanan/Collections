56d79905ef67a3382e5c10829e1d5404dcd060d9
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-02-18 10:34:38 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Unitialized
+            string[] stringArray0 = new string[3];
+            // Initialized
+            string[] stringArray1 = { "This ", "is ", "SPARTA . . . er, an array, I mean!\n" };
+
+            // Unitialized list
+            List<string> stringList = new List<string>();
+
+            stringList.Add("This ");
+            stringList.Add("is a ");
+            stringList.Add("LISTA!\n");
+
+            for (int i = 0; i < stringArray1.Length; i++)
+            {
+                Console.Write(stringArray1[i]);
+            }
+
+            Console.WriteLine();
+
+            foreach (string str in stringList)
+            {
+                Console.Write(str);
+            }
+
+            Console.WriteLine();
+
+            /* --------------------------- */
+
+            stringList.Insert(1, "container ");
+
+            foreach (string str in stringList)
+            {
+                Console.Write(str);
+            }
+
+
+        }
+    }
+}
